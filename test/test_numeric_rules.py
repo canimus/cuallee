8cfba6cdfcffa051bc8c9c5652e34a2bccadd186
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-09-20 18:35:44 +0200

    fix tests for is_contained_in

--- test/test_numeric_rules.py
+++ test/test_numeric_rules.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from cuallee import Check,  CheckLevel
+from cuallee import Check, CheckLevel
 
 def test_are_complete(spark: SparkSession):
     df = spark.createDataFrame([[1, 2], [None, 1]], ['A', 'B'])
@@ -24,9 +24,18 @@ def test_matches_regex(spark: SparkSession):
     rs = c.validate(spark, df)
     assert rs.select('status').collect()[0][0] == False
 
-def test_is_contained_in(spark: SparkSession):
-    df = spark.createDataFrame([[1, 10], [2, 15], [3, 17]], ['ID', 'values'])
-    c = Check(CheckLevel.WARNING, 'is_contained_in_test')
-    c.is_contained_in('values', (10,15,20,25))
+def test_string_is_contained_in(spark: SparkSession):
+    df = spark.createDataFrame([[1, 'blue'], [2, 'green'], [3, 'grey']], ['ID', 'desc'])
+    c = Check(CheckLevel.WARNING, 'is_contained_in_string_test')
+    c.is_contained_in('desc', ('blue','red'))
     rs = c.validate(spark, df)
     assert rs.select('status').collect()[0][0] == False
+
+def test_number_is_contained_in(spark: SparkSession):
+    df = spark.createDataFrame([[1, 10], [2, 15], [3, 17]], ['ID', 'value'])
+    c = Check(CheckLevel.WARNING, 'is_contained_in_number_test')
+    c.is_contained_in('value', (10, 15, 20, 25))
+    rs = c.validate(spark, df)
+    assert rs.select('status').collect()[0][0] == False
+
+

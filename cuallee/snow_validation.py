dd8cf227eee36604a8aedcfd1ab5c9570efa0b2d
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-10-16 19:00:14 +0000

    add first method is_complete

--- cuallee/snow_validation.py
+++ cuallee/snow_validation.py
@@ -1,10 +1,48 @@
+import enum
 import snowflake.snowpark.functions as F
 import snowflake.snowpark.types as T
 
-from typing import Dict
-from snowflake.snowpark import DataFrame
+from typing import Union, Dict
+from dataclasses import dataclass
+from snowflake.snowpark import DataFrame, Column
 
 from cuallee import Check, Rule
+
+
+class ComputeMethod(enum.Enum):
+    SELECT = 'SELECT'
+    TRANSFORM = 'TRANSFORM'
+
+
+@dataclass
+class ComputeInstruction:
+    predicate: Union[Column, None]
+    expression: Column
+    compute_method: ComputeMethod
+
+    def __repr__(self):
+        return f"ComputeInstruction({self.compute_method})"
+
+
+class Compute:
+    def __init__(self):
+        self.compute_instruction = ComputeInstruction
+
+    def __repr__(self):
+        return self.compute_instruction
+
+    def _sum_predicate_to_integer(self, predicate: Column):
+        return F.sum(predicate.cast("integer"))
+
+    def is_complete(self, rule: Rule):
+        """Validation for non-null values in column"""
+        predicate = F.col(f"`{rule.column}`").isNotNull()
+        self.compute_instruction = ComputeInstruction(
+            predicate,
+            self._sum_predicate_to_integer(predicate),
+            ComputeMethod.SELECT,
+        )
+        return self.compute_instruction
 
 
 def compute(rules: Dict[str, Rule]):
@@ -13,7 +51,7 @@ def compute(rules: Dict[str, Rule]):
 def validate_data_types(rules: Dict[str, Rule], dataframe: DataFrame):
     return 'I validate DataTypes'
 
-def summary(check: Check, dataframe):
+def summary(check: Check, dataframe: DataFrame):
     """Compute all rules in this check for specific data frame"""
     return "I am a Snow DataFrame!"
 

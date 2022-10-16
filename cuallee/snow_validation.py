1dde645726f36bef33b394403e346ee671b3107a
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-10-16 18:44:48 +0000

    Initiate the compute, validate types and summary functions

--- cuallee/snow_validation.py
+++ cuallee/snow_validation.py
@@ -1,6 +1,19 @@
-from cuallee import Check
+import snowflake.snowpark.functions as F
+import snowflake.snowpark.types as T
+
+from typing import Dict
+from snowflake.snowpark import DataFrame
+
+from cuallee import Check, Rule
 
 
-def compute_summary(dataframe, check: Check):
+def compute(rules: Dict[str, Rule]):
+    return 'Compute dictionnary'
+
+def validate_data_types(rules: Dict[str, Rule], dataframe: DataFrame):
+    return 'I validate DataTypes'
+
+def summary(check: Check, dataframe):
     """Compute all rules in this check for specific data frame"""
-    return "I am a Pandas DataFrame!"
+    return "I am a Snow DataFrame!"
+

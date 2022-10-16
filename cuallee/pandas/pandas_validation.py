5f5ac93fa58b50bdbf3f464884d76456382f0f40
Herminio Vazquez <canimus@gmail.com>
2022-10-16 23:21:26 +0200

    Removal of unnecessary packages

--- cuallee/pandas/pandas_validation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd  # type: ignore
-
-from cuallee import Check
-
-
-def pd_compute_summary(dataframe: pd.DataFrame, check: Check) -> pd.DataFrame:
-    """Compute all rules in this check for specific data frame"""
-    return "I am a Pandas DataFrame!"
--- cuallee/snowspark/snow_validation.py
+++
@@ -1,6 +0,0 @@
-from cuallee import Check
-
-
-def compute_summary(dataframe, check: Check):
-    """Compute all rules in this check for specific data frame"""
-    return "I am a Pandas DataFrame!"

450e41f0eae23f0e7f7aa7f3e6029f14e37007b1
Herminio Vazquez <canimus@gmail.com>
2022-10-17 02:07:40 +0200

    Pandas validation first attempt

--- cuallee/pandas_validation.py
+++ cuallee/pandas_validation.py
@@ -3,11 +3,14 @@ from cuallee import Check, Rule
 import pandas as pd  # type: ignore
 import operator
 
+
 class Compute:
     def is_complete(self, rule: Rule):
         def _execute(dataframe: pd.DataFrame):
             return dataframe.loc[:, rule.column].isnull().sum()
+
         return _execute
+
 
 def compute(rules: Dict[str, Rule]):
     return {k: operator.methodcaller(v.method, v)(Compute()) for k, v in rules.items()}
@@ -19,4 +22,6 @@ def validate_data_types(rules: Dict[str, Rule], dataframe: pd.DataFrame):
 
 def summary(check: Check, dataframe: pd.DataFrame):
     computation = compute(check._rule)
-    return pd.DataFrame({rule.method: [computation[rule.key](dataframe)] for rule in check.rules})
+    return pd.DataFrame(
+        {rule.method: [computation[rule.key](dataframe)] for rule in check.rules}
+    )

de6da9de3d013d4361d565a2f1f1dac274c49f34
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-10-22 15:23:46 +0200

    working test cases for is_complete & are_complete

---
+++ test/unit/snowflake/test_are_complete.py
@@ -0,0 +1,14 @@
+import snowflake.snowpark.functions as F  # type: ignore
+
+from snowflake.snowpark import DataFrame  # type: ignore
+from cuallee import Check, CheckLevel
+
+
+def test_are_complete(snowpark, configurations):
+    df = snowpark.range(10).withColumn("desc", F.col("id").cast("string"))
+    check = Check(CheckLevel.WARNING, "check_are_complete")
+    check.are_complete(("ID", "DESC"))
+    check.config = configurations
+    rs = check.validate(df)
+    assert isinstance(rs, DataFrame)
+    assert rs.first().STATUS == "PASS"

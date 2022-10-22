900be9b235da027c0973461909c3c944dcaee61f
Herminio Vazquez <canimus@gmail.com>
2022-10-22 19:53:22 +0200

    Added are complete and unique test cases

---
+++ test/unit/pandas_dataframe/test_are_complete.py
@@ -0,0 +1,20 @@
+from cuallee import Check
+import pandas as pd
+
+def test_numeric_columns(check: Check):
+    check.are_complete(("id", "id2"))
+    df = pd.DataFrame({"id" : [10,20], "id2" : [300,500]})
+    assert check.validate(df).status.str.match("PASS").all()
+
+def test_empty_columns(check: Check):
+    check.are_complete(("id", "id2"))
+    df = pd.DataFrame({"id" : [10,None], "id2" : [300,500]})
+    assert check.validate(df).status.str.match("FAIL").all()
+
+
+def test_coverage(check: Check):
+    check.are_complete(("id", "id2"), 0.75)
+    df = pd.DataFrame({"id" : [10,None], "id2" : [300,500]})
+    result = check.validate(df)
+    assert result.status.str.match("PASS").all()
+    assert result.pass_rate.max() == 0.75

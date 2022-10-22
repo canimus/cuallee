6ceb6f3ff7a4312e18e90cdc0f0197e9380488f3
Herminio Vazquez <canimus@gmail.com>
2022-10-22 22:58:25 +0200

    pass is billions

--- test/unit/pandas_dataframe/test_is_in_billions.py
+++ test/unit/pandas_dataframe/test_is_in_billions.py
@@ -1,2 +1,17 @@
-def test_below_range():
-    pass
+from cuallee import Check
+import pandas as pd
+
+def test_in_range(check: Check):
+    check.is_in_billions("id")
+    df = pd.DataFrame({"id" : [1e9, 1e9+1]})
+    assert check.validate(df).status.eq("PASS").all()
+
+def test_below_range(check: Check):
+    check.is_in_billions("id")
+    df = pd.DataFrame({"id" : [1, 2]})
+    assert check.validate(df).status.eq("FAIL").all()
+
+def test_coverage(check: Check):
+    check.is_in_billions("id", 0.5)
+    df = pd.DataFrame({"id" : [1.0, 1e9]})
+    assert check.validate(df).status.eq("PASS").all()

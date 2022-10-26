e8443977c59cfcad41497dfb90dbee941bd02eda
Herminio Vazquez <canimus@gmail.com>
2022-10-26 21:34:24 +0200

    Added sql examples

---
+++ cuallee/sql_validation.py
@@ -0,0 +1,14 @@
+from typing import Dict, Union, List
+from cuallee import Check, Rule
+import duckdb as dk
+import operator
+import numpy as np
+import re
+from toolz import first  # type: ignore
+from numbers import Number
+from cuallee import utils as cuallee_utils
+from pyspark.sql.session import SparkSession
+
+class Compute:
+    def is_complete(self, rule: Rule, connection: Union[dk.DuckDBPyConnection, SparkSession]) -> Union[bool, int]:
+        return connection.execute(f"SELECT sum(cast({rule.column} IS NULL AS INTEGER)) from {rule.table}")

105a11eab873d09cb3e612cca085a359b34f8b70
Herminio Vazquez <canimus@gmail.com>
2022-10-29 22:44:59 +0200

    Added first pandas test

--- test/unit/pandas_dataframe/test_has_correlation.py
+++ test/unit/pandas_dataframe/test_has_correlation.py
@@ -3,22 +3,17 @@ import pandas as pd
 import numpy as np
 
 
-def test_numeric_columns(check: Check):
+def test_positive(check: Check):
     check.has_correlation("id", "id2", 1.0)
     df = pd.DataFrame({"id": [10, 20], "id2": [100, 200]})
     assert check.validate(df).status.str.match("PASS").all()
 
 
-def test_large_matrix(check: Check):
-    check.has_correlation("id", "id2", 1.0)
-    df = pd.DataFrame({"id": np.arange(100), "id2": np.arange(100) * 10})
-    assert check.validate(df).status.eq("PASS").all()
-
-
-def test_empty_columns(check: Check):
+def test_negative(check: Check):
     check.has_correlation("id", "id2", 1.0)
     df = pd.DataFrame({"id": [10, 20, 30, None], "id2": [100, 200, 300, 400]})
     assert check.validate(df).status.eq("PASS").all()
+
 
 
 def test_coverage(check: Check):

d6bcab35acb4a08ba9de345822d6b88af7703333
Herminio Vazquez <canimus@gmail.com>
2022-10-31 22:07:47 +0100

    Has min by correction on pandas

--- test/unit/pandas_dataframe/test_has_max_by.py
+++ test/unit/pandas_dataframe/test_has_max_by.py
@@ -19,8 +19,6 @@ def test_values(check: Check, answer, columns):
     assert check.validate(df).status.str.match("PASS").all()
 
 def test_coverage(check: Check):
-    check.are_complete(("id", "id2"), 0.75)
-    df = pd.DataFrame({"id": [10, None], "id2": [300, 500]})
-    result = check.validate(df)
-    assert result.status.str.match("PASS").all()
-    assert result.pass_rate.max() == 0.75
+    with pytest.raises(TypeError):
+        check.has_max_by("id", "id2", 20, 100)
+    

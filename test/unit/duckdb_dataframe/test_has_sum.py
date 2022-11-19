a6c666a1ee2b29f071a8c09281161e406cef8a68
Herminio Vazquez <canimus@gmail.com>
2022-11-19 12:13:12 +0000

    Added specific tests for duckdb

--- test/unit/duckdb_dataframe/test_has_sum.py
+++ test/unit/duckdb_dataframe/test_has_sum.py
@@ -8,3 +8,14 @@ def test_positive(check: Check):
     conn = duckdb.connect(":memory:")
     conn.execute("CREATE TABLE TEMP AS SELECT UNNEST(RANGE(10)) AS ID")
     assert check.validate(conn).status.str.match("PASS").all()
+
+def test_negative(check: Check):
+    check.has_sum("id", 40)
+    check.table_name = "TEMP"
+    conn = duckdb.connect(":memory:")
+    conn.execute("CREATE TABLE TEMP AS SELECT UNNEST(RANGE(10)) AS ID")
+    assert check.validate(conn).status.str.match("FAIL").all()
+
+def test_coverage(check: Check):
+    with pytest.raises(TypeError):
+        check.has_sum("id", 10, 0.5)

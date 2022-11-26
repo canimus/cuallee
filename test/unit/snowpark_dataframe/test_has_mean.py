97d389d7371b6ad0db9f8b42fe5b30e8f8f2cdd8
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-11-26 11:51:35 +0100

    complete test cases for has_mean snowpark

--- test/unit/snowpark_dataframe/test_has_mean.py
+++ test/unit/snowpark_dataframe/test_has_mean.py
@@ -1,12 +1,36 @@
-from snowflake.snowpark import DataFrame  # type: ignore
+import pytest
+
 from cuallee import Check, CheckLevel
 
 
-def test_stats_average_value(snowpark, configurations):
+def test_positive(snowpark):
     df = snowpark.range(10)
-    check = Check(CheckLevel.WARNING, "check_has_mean")
+    check = Check(CheckLevel.WARNING, "pytest")
     check.has_mean("ID", 4.5)
-    check.config = configurations
     rs = check.validate(df)
-    assert (rs, DataFrame)
     assert rs.first().STATUS == "PASS"
+
+
+def test_negative(snowpark):
+    df = snowpark.range(10)
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_mean("ID", 3.5)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "FAIL"
+
+
+@pytest.mark.parametrize("rule_value", [int(1), float(1.0)], ids=["int", "float"])
+def test_parameters(snowpark, rule_value):
+    df = snowpark.createDataFrame(
+        [[1], [1], [1], [1], [1], [1], [1], [1], [1], [1]], ["test"]
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_mean("TEST", rule_value)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "PASS"
+
+
+def test_coverage():
+    check = Check(CheckLevel.WARNING, "pytest")
+    with pytest.raises(TypeError, match="positional arguments"):
+        check.has_mean("ID", 4.5, 0.5)

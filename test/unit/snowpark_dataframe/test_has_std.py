020dde948c383a48e198c234bfa82791066c6d6c
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-11-26 16:18:42 +0100

    complete tes cases has_std snowpark

--- test/unit/snowpark_dataframe/test_has_std.py
+++ test/unit/snowpark_dataframe/test_has_std.py
@@ -1,12 +1,36 @@
-from snowflake.snowpark import DataFrame  # type: ignore
+import pytest
+
 from cuallee import Check, CheckLevel
 
 
-def test_stats_std_value(snowpark, configurations):
+def test_positive(snowpark):
     df = snowpark.range(10)
-    check = Check(CheckLevel.WARNING, "check_has_std")
-    check.has_std("ID", 2.5)
-    check.config = configurations
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_std("ID", 2.8722813232690143)
     rs = check.validate(df)
-    assert (rs, DataFrame)
+    assert rs.first().STATUS == "PASS"
+
+
+def test_negative(snowpark):
+    df = snowpark.range(10)
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_std("ID", 2.5)
+    rs = check.validate(df)
     assert rs.first().STATUS == "FAIL"
+
+
+@pytest.mark.parametrize("data_type", [[[int(0)], [int(1)], [int(2)], [int(3)], [int(4)], [int(5)], [int(6)], [int(7)], [int(8)], [int(9)]], [[float(0.0)], [float(1.0)], [float(2.0)], [float(3.0)], [float(4.0)], [float(5.0)], [float(6.0)], [float(7.0)], [float(8.0)], [float(9.0)]]], ids=["int", "float"])
+def test_parameters(snowpark, data_type):
+    df = snowpark.createDataFrame(
+        data_type, ["ID"]
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_std("ID", 2.8722813232690143)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "PASS"
+
+
+def test_coverage():
+    check = Check(CheckLevel.WARNING, "pytest")
+    with pytest.raises(TypeError, match="positional arguments"):
+        check.has_std("ID", 2.8722813232690143, 0.7)

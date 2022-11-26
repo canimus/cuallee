6c66da7efe1374a61af59793e956edae8dd7a00d
Herminio Vazquez <canimus@gmail.com>
2022-11-27 00:43:49 +0100

    Clean up test folders

--- test/unit/duckdb_dataframe/test_has_sum.py
+++ test/unit/duckdb_dataframe/test_has_sum.py
@@ -2,19 +2,15 @@ import duckdb
 from cuallee import Check
 import pytest
 
-def test_positive(check: Check):    
+def test_positive(check: Check, db: duckdb.DuckDBPyConnection):    
     check.has_sum("id", 45)
     check.table_name = "TEMP"
-    conn = duckdb.connect(":memory:")
-    conn.execute("CREATE TABLE TEMP AS SELECT UNNEST(RANGE(10)) AS ID")
-    assert check.validate(conn).status.str.match("PASS").all()
+    assert check.validate(db).status.str.match("PASS").all()
 
-def test_negative(check: Check):
+def test_negative(check: Check, db: duckdb.DuckDBPyConnection):
     check.has_sum("id", 40)
     check.table_name = "TEMP"
-    conn = duckdb.connect(":memory:")
-    conn.execute("CREATE TABLE TEMP AS SELECT UNNEST(RANGE(10)) AS ID")
-    assert check.validate(conn).status.str.match("FAIL").all()
+    assert check.validate(db).status.str.match("FAIL").all()
 
 def test_coverage(check: Check):
     with pytest.raises(TypeError):

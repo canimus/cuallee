e66a2828893491dc5a7fd79f5b0d440fc7b676ba
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-11-26 16:07:27 +0100

    complete test case has_percentile snowpark

--- test/unit/snowpark_dataframe/test_has_percentile.py
+++ test/unit/snowpark_dataframe/test_has_percentile.py
@@ -1,34 +1,50 @@
+import pytest
 import snowflake.snowpark.functions as F  # type: ignore
 
-from snowflake.snowpark import DataFrame, Row  # type: ignore
 from cuallee import Check, CheckLevel
 
 
-def test_has_percentile(snowpark, configurations):
+def test_positive(snowpark):
     df = snowpark.range(10)
-    check = Check(CheckLevel.ERROR, "check_has_percentile")
+    check = Check(CheckLevel.WARNING, "pytest")
     check.has_percentile("ID", 4.5, 0.5)
-    check.config = configurations
     rs = check.validate(df)
-    assert isinstance(rs, DataFrame)
     assert rs.first().STATUS == "PASS"
 
 
-def test_has_percentile_with_precision(snowpark, configurations):
+def test_negative(snowpark):
     df = snowpark.range(10)
-    check = Check(CheckLevel.ERROR, "check_has_percentile_with_precision")
-    check.has_percentile("ID", 4.5, 0.5, 1000)
-    check.config = configurations
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_percentile("ID", 4.0, 0.5)
     rs = check.validate(df)
-    assert isinstance(rs, DataFrame)
+    assert rs.first().STATUS == "FAIL"
+
+
+@pytest.mark.parametrize("data_type, rule_value", [[[[0], [2], [4]], int(3)], [[[0], [2], [4]], float(3.0)], [[[float(0.0)], [float(2.0)], [float(4.0)]], int(3)], [[[float(0.0)], [float(2.0)], [float(4.0)]], float(3.0)]], ids=["value_int", "value_float", "data_int", "data_float"])
+def test_parameters(snowpark, data_type, rule_value):
+    df = snowpark.createDataFrame(
+        data_type, ["TEST"]
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_percentile("TEST", rule_value, 0.5)
+    rs = check.validate(df)
     assert rs.first().STATUS == "PASS"
 
 
-# def test_has_percentile_with_pct(snowpark, configurations):
-#    df = snowpark.range(10)
-#    check = Check(CheckLevel.ERROR, "check_has_percentile_with_pct")
-#    check.has_percentile("ID", 4.5, 0.5, 1000, 0.8)
-#    check.config = configurations
-#    rs = check.validate(df)
-#    assert isinstance(rs, DataFrame)
-#    assert rs.first().STATUS == "PASS"
+def test_coverage():
+    check = Check(CheckLevel.WARNING, "pytest")
+    with pytest.raises(TypeError, match="positional arguments"):
+        check.has_percentile("ID", 3.0, 0.5, 1000, 0.7)
+
+
+@pytest.mark.parametrize(
+    "percentage, precision, expected",
+    [[0.5, 1000, "PASS"], [1.0, 1000, "FAIL"], [0.5, 100000000, "PASS"]], 
+    ids=["percentage_pass", "percentage_fail", "precision"],
+)
+def test_settings(snowpark, percentage, precision, expected): 
+    df = snowpark.range(10)
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.has_percentile("ID", 4.5, percentage, precision)
+    rs = check.validate(df)
+    assert rs.first().STATUS == expected

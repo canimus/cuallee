fda6cf34761fcf26e5300697e8cc7e4e0b4c7a55
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-11-30 16:02:16 +0100

    complete test cases is_equal_than snowpark

--- test/unit/snowpark_dataframe/test_is_equal_than.py
+++ test/unit/snowpark_dataframe/test_is_equal_than.py
@@ -1,14 +1,52 @@
+import pytest
 import snowflake.snowpark.functions as F  # type: ignore
 
 from snowflake.snowpark import DataFrame  # type: ignore
 from cuallee import Check, CheckLevel
 
 
-def test_value_equal_than(snowpark, configurations):
-    df = snowpark.range(10).withColumn("value", F.lit(1000))
-    check = Check(CheckLevel.WARNING, "check_is_equal_than")
+def test_positive(snowpark):
+    df = snowpark.range(10).withColumn("VALUE", F.lit(1000))
+    check = Check(CheckLevel.WARNING, "pytest")
     check.is_equal_than("VALUE", 1000)
-    check.config = configurations
     rs = check.validate(df)
-    assert isinstance(rs, DataFrame)
     assert rs.first().STATUS == "PASS"
+    assert rs.first().VIOLATIONS == 0
+    assert rs.first().PASS_THRESHOLD == 1.0
+
+
+def test_negative(snowpark):
+    df = snowpark.createDataFrame(
+        [[1000], [1], [1000], [1000], [10]], ["VALUE"]
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_equal_than("VALUE", 1000)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "FAIL"
+    assert rs.first().VIOLATIONS == 2
+    assert rs.first().PASS_THRESHOLD == 1.0
+    assert rs.first().PASS_RATE == 3/5
+
+
+@pytest.mark.parametrize(
+    "rule_value", [int(1000), float(1000)], ids=("int", "float")
+)
+def test_parameters(snowpark, rule_value):
+    df = snowpark.range(10).withColumn("VALUE", F.lit(1000))
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_equal_than("VALUE", rule_value)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "PASS"
+
+
+def test_coverage(snowpark):
+    df = snowpark.createDataFrame(
+        [[1000], [1], [1000], [1000], [10]], ["VALUE"]
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_equal_than("VALUE", 1000, 0.6)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "PASS"
+    assert rs.first().VIOLATIONS == 2
+    assert rs.first().PASS_THRESHOLD == 0.6
+    assert rs.first().PASS_RATE == 3/5

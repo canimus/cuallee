8649f8576c705be4a2ad4c05cd2d49b495238e12
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-12-03 11:54:28 +0100

    complete test cases is_on_weekend sf

--- test/unit/snowpark_dataframe/test_is_on_weekend.py
+++ test/unit/snowpark_dataframe/test_is_on_weekend.py
@@ -1,29 +1,45 @@
 import snowflake.snowpark.functions as F  # type: ignore
 
-from snowflake.snowpark import DataFrame  # type: ignore
 from cuallee import Check, CheckLevel
 
 
 def test_positive(snowpark):
-    df = snowpark.range(5).withColumn(
-        "ARRIVAL_DATE", F.date_from_parts(2022, 11, F.col("id") + 14)
+    df = snowpark.range(2).withColumn(
+        "ARRIVAL_DATE", F.date_from_parts(2022, 11, F.col("id") + 12)
     )
     check = Check(CheckLevel.WARNING, "pytest")
-    check.is_on_weekday("ARRIVAL_DATE")
+    check.is_on_weekend("ARRIVAL_DATE")
     rs = check.validate(df)
     assert rs.first().STATUS == "PASS"
     assert rs.first().VIOLATIONS == 0
     assert rs.first().PASS_THRESHOLD == 1.0
     
 
-def test_is_on_weekend(snowpark, configurations):
-    df = snowpark.range(10).withColumn(
-        "date", F.date_from_parts(2022, 1, F.col("id") + 1)
+def test_negative(snowpark):
+    df = snowpark.range(5).withColumn(
+        "ARRIVAL_DATE", F.date_from_parts(2022, 11, F.col("id") + 12)
     )
-    check = Check(CheckLevel.WARNING, "check_is_on_weekend")
-    check.is_on_weekend("DATE")
-    check.config = configurations
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_on_weekend("ARRIVAL_DATE")
     rs = check.validate(df)
-    assert isinstance(rs, DataFrame)
     assert rs.first().STATUS == "FAIL"
-    assert rs.first().VIOLATIONS == 6, "Incorrect calulation of Weekend filters"
+    assert rs.first().VIOLATIONS == 3
+    assert rs.first().PASS_THRESHOLD == 1.0
+    assert rs.first().PASS_RATE == 2/5
+    
+
+def test_parameters(snowpark):
+    return "😅 No parameters to be tested!"
+
+
+def test_coverage(snowpark):
+    df = snowpark.range(9).withColumn(
+        "ARRIVAL_DATE", F.date_from_parts(2022, 11, F.col("id") + 12)
+    )
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_on_weekend("ARRIVAL_DATE", 0.4)
+    rs = check.validate(df)
+    assert rs.first().STATUS == "PASS"
+    assert rs.first().VIOLATIONS == 5
+    assert rs.first().PASS_THRESHOLD == 0.4
+    assert rs.first().PASS_RATE == 4/9

b36dd82cb59a31b2aaeed7ad6492d752a2893cda
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-12-07 17:34:28 +0100

    complete test cases is_greater_or_equal_than spark

--- test/unit/pyspark_dataframe/test_is_greater_or_equal_than.py
+++ test/unit/pyspark_dataframe/test_is_greater_or_equal_than.py
@@ -1,2 +1,45 @@
-def test_bigger_number(spark):
-    pass
+import pytest
+import pyspark.sql.functions as F
+
+from cuallee import Check, CheckLevel
+
+
+def test_positive(spark):
+    df = spark.range(10).withColumn("VALUE", F.col("id") + 1000)
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_greater_or_equal_than("VALUE", 1000)
+    rs = check.validate(df)
+    assert rs.first().status == "PASS"
+    assert rs.first().violations == 0
+    assert rs.first().pass_threshold == 1.0
+
+
+def test_negative(spark):
+    df = spark.createDataFrame([[1006], [1], [1070], [1900], [10]], ["VALUE"])
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_greater_or_equal_than("VALUE", 1000)
+    rs = check.validate(df)
+    assert rs.first().status == "FAIL"
+    assert rs.first().violations == 2
+    assert rs.first().pass_threshold == 1.0
+    assert rs.first().pass_rate == 3 / 5
+
+
+@pytest.mark.parametrize("rule_value", [int(1000), float(1000)], ids=("int", "float"))
+def test_parameters(spark, rule_value):
+    df = spark.range(10).withColumn("VALUE", F.col("id") + 1000)
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_greater_or_equal_than("VALUE", rule_value)
+    rs = check.validate(df)
+    assert rs.first().status == "PASS"
+
+
+def test_coverage(spark):
+    df = spark.createDataFrame([[1006], [1], [1070], [1900], [10]], ["VALUE"])
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_greater_or_equal_than("VALUE", 1000, 0.6)
+    rs = check.validate(df)
+    assert rs.first().status == "PASS"
+    assert rs.first().violations == 2
+    assert rs.first().pass_threshold == 0.6
+    assert rs.first().pass_rate == 3 / 5

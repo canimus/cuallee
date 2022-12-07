e53276e8db8079db557845a51a1b3fbf2bcc6c10
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-12-07 17:52:43 +0100

    complete test cases is_unique spark

--- test/unit/pyspark_dataframe/test_is_unique.py
+++ test/unit/pyspark_dataframe/test_is_unique.py
@@ -1,8 +1,38 @@
+import pytest
+import pyspark.sql.functions as F
+
 from cuallee import Check, CheckLevel
 
 
-def test_unique_column(spark):
+def test_positive(spark):
     df = spark.range(10)
-    check = Check(CheckLevel.WARNING, "UniqueColumn")
+    check = Check(CheckLevel.WARNING, "pytest")
     check.is_unique("id")
-    assert check.validate(df).first().status == "PASS"
+    rs = check.validate(df)
+    assert rs.first().status == "PASS"
+
+
+def test_negative(spark):
+    df = spark.createDataFrame([[0], [2], [2], [3]], ["id"])
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_unique("id")
+    rs = check.validate(df)
+    assert rs.first().status == "FAIL"
+    assert rs.first().violations == 1
+    assert rs.first().pass_threshold == 1.0
+    assert rs.first().pass_rate == 3 / 4
+
+
+def test_parameters(spark):
+    return "😅 No parameters to be tested!"
+
+
+def test_coverage(spark):
+    df = spark.createDataFrame([[0], [2], [2], [3]], ["id"])
+    check = Check(CheckLevel.WARNING, "pytest")
+    check.is_unique("id", 0.7)
+    rs = check.validate(df)
+    assert rs.first().status == "PASS"
+    assert rs.first().violations == 1
+    assert rs.first().pass_threshold == 0.7
+    assert rs.first().pass_rate == 3 / 4

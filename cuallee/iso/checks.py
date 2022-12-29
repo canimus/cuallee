fa8db94f3c2ba7e7cb004f08d94b4c7b532b7669
Herminio Vazquez <canimus@gmail.com>
2022-12-29 10:08:49 +0100

    Added the first currency check on iso standard

--- cuallee/iso/checks.py
+++ cuallee/iso/checks.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from typing import List
 import os
 from operator import attrgetter as at
+from functools import lru_cache
 
 @dataclass
 class Currency:
@@ -18,11 +19,9 @@ class ISO():
     CCY_CODE = "currency"
     CCY_NUMBER = "currency_number"
 
-    def __init__(self, check):
-        self._check = check
-
-    def iso_4217(self, column : str, by_field : str = CCY_CODE):
-        """It verifies a field against the international standard currency codes via code or number fields from ISO 4217"""
+    @lru_cache
+    def _load_currencies(self, by_field : str = CCY_CODE):
+        """Internal helper method to load all currencies from default environment location"""
         load_dotenv()
         response = requests.get(os.getenv("ISO_4217_ENDPOINT"))
         xml = etree.fromstring(response.text.encode("utf-8"))
@@ -30,6 +29,13 @@ class ISO():
         def _get_ccy(element : etree._Element) -> Currency:
             return at(by_field)(Currency(*[tag.text for tag in element.getchildren()]))
 
-        codes = set(filter(None, (map(_get_ccy, xml.xpath("//CcyNtry")))))
-        self._check.is_in(column, codes)
+        return set(filter(None, (map(_get_ccy, xml.xpath("//CcyNtry")))))
+
+    def __init__(self, check, currency_field : str = CCY_CODE):
+        self._check = check
+        self._ccy = self._load_currencies(currency_field)
+
+    def iso_4217(self, column : str):
+        """It verifies a field against the international standard currency codes via code or number fields from ISO 4217"""
+        self._check.is_in(column, self._ccy)
     

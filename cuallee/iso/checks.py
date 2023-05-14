8007ed9850f43101700b6e691fdc12ee131586b7
Herminio Vazquez <canimus@gmail.com>
2023-05-14 13:11:38 +0200

    Proper execution of ISO checks

--- cuallee/iso/checks.py
+++ cuallee/iso/checks.py
@@ -5,6 +5,8 @@ from typing import List
 import os
 from operator import attrgetter as at
 from functools import lru_cache
+import pandas as pd
+from toolz import first
 
 @dataclass
 class Currency:
@@ -15,30 +17,49 @@ class Currency:
     currency_units: str = None
 
 @lru_cache
-def _load_currencies(by_field : str):
-    """Internal helper method to load all currencies from default environment location"""
-    DEFAULT_ENDPOINT = "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
-    response = requests.get(os.getenv("ISO_4217_ENDPOINT", DEFAULT_ENDPOINT))
+def _load_currencies():
+    """ External download from ISO website of currencies in XML format """
+    DEFAULT_ENDPOINT_4217 = "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
+    response = requests.get(os.getenv("ISO_4217_ENDPOINT", DEFAULT_ENDPOINT_4217))
     xml = etree.fromstring(response.text.encode("utf-8"))
 
     def _get_ccy(element : etree._Element) -> Currency:
-        return at(by_field)(Currency(*[tag.text for tag in element.getchildren()]))
+        return at("currency")(Currency(*[tag.text for tag in element.getchildren()]))
 
     return set(filter(None, (map(_get_ccy, xml.xpath("//CcyNtry")))))
 
+@lru_cache
+def _load_countries():
+    """ External download from Google shared data of country codes and locations """
+    DEFAULT_ENDPOINT_3166 = "https://developers.google.com/public-data/docs/canonical/countries_csv"
+    response = pd.read_html(os.getenv("ISO_3166_ENDPOINT", DEFAULT_ENDPOINT_3166))
+    return first(response)["country"].str.upper().dropna().tolist()
+
+
 class ISO():
-    # These options mean what is the format expected to validate your currency field
-    # which could be either the currency name or the code
+    """ Abstraction of checks related to ISO standards """
     CCY_CODE = "currency"
     CCY_NUMBER = "currency_number"
+    COUNTRY_CODE = "country"
+    COUNTRY_NAME = "name"
 
-    def __init__(self, check, currency_field : str = CCY_CODE):
+    def __init__(self, check):
         self._check = check
-        self._field = currency_field
         self._ccy = []
+        self._countries = []
 
     def iso_4217(self, column : str):
         """It verifies a field against the international standard currency codes via code or number fields from ISO 4217"""
-        self._ccy = _load_currencies(self._field)
+        self._ccy = _load_currencies()
         self._check.is_in(column, self._ccy)
+        return self._check
     
+    def iso_3166(self, column: str):
+        """ Verifies that country codes are valid against the ISO standard 3166 """
+        self._countries = _load_countries()
+        self._check.is_in(column, self._countries)
+        return self._check
+    
+
+    iso_currencies = iso_4217
+    iso_countries = iso_3166

d4650d07c8e3453036a398d85d2c2cd64dd533a9
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-08-27 17:14:52 +0000

    black, flake and type ok on bq

--- test/unit/bigquery/test_is_complete.py
+++ test/unit/bigquery/test_is_complete.py
@@ -6,21 +6,21 @@ from cuallee import Check, CheckLevel
 
 
 def test_positive():
-    df = bigquery.dataset.Table('bigquery-public-data.chicago_taxi_trips.taxi_trips')
+    df = bigquery.dataset.Table("bigquery-public-data.chicago_taxi_trips.taxi_trips")
     check = Check(CheckLevel.WARNING, "pytest")
     check.is_complete("taxi_id")
     rs = check.validate(df)
-    assert rs.status.str.match('PASS')[1]
+    assert rs.status.str.match("PASS")[1]
     assert rs.violations[1] == 0
     assert rs.pass_rate[1] == 1.0
 
 
 def test_negative():
-    df = bigquery.dataset.Table('bigquery-public-data.chicago_taxi_trips.taxi_trips')
+    df = bigquery.dataset.Table("bigquery-public-data.chicago_taxi_trips.taxi_trips")
     check = Check(CheckLevel.WARNING, "pytest")
     check.is_complete("trip_end_timestamp")
     rs = check.validate(df)
-    assert rs.status.str.match('FAIL')[1]
+    assert rs.status.str.match("FAIL")[1]
     assert rs.violations[1] == 18434
     assert rs.pass_threshold[1] == 1.0
     assert rs.pass_rate[1] == 0.9999117752439066
@@ -31,11 +31,11 @@ def test_negative():
 
 
 def test_coverage():
-    df = bigquery.dataset.Table('bigquery-public-data.chicago_taxi_trips.taxi_trips')
+    df = bigquery.dataset.Table("bigquery-public-data.chicago_taxi_trips.taxi_trips")
     check = Check(CheckLevel.WARNING, "pytest")
     check.is_complete("trip_end_timestamp", 0.7)
     rs = check.validate(df)
-    assert rs.status.str.match('PASS')[1]
+    assert rs.status.str.match("PASS")[1]
     assert rs.violations[1] == 18434
     assert rs.pass_threshold[1] == 0.7
-    assert rs.pass_rate[1] == 0.9999117752439066  #207158222/207176656
+    assert rs.pass_rate[1] == 0.9999117752439066  # 207158222/207176656

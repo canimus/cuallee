59bb418496648e9377f6b3c611ca3665d579ea1d
Herminio Vazquez <canimus@gmail.com>
2023-10-01 18:25:34 +0200

    Formated and refactored

--- cuallee/polars_validation.py
+++ cuallee/polars_validation.py
@@ -20,8 +20,14 @@ class Compute:
 
     @staticmethod
     def _value(dataframe: pl.DataFrame):
-        return compose(first, first, list, operator.methodcaller("values"), operator.methodcaller("to_dict", as_series=False))(dataframe)
-    
+        return compose(
+            first,
+            first,
+            list,
+            operator.methodcaller("values"),
+            operator.methodcaller("to_dict", as_series=False),
+        )(dataframe)
+
     def is_complete(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
         """Validate not null"""
         return Compute._result(
@@ -145,9 +151,7 @@ class Compute:
         low, high = rule.value
         return Compute._result(
             dataframe.select(
-                pl.col(rule.column)
-                .is_between(low, high, closed="both")
-                .cast(pl.Int8)
+                pl.col(rule.column).is_between(low, high, closed="both").cast(pl.Int8)
             ).sum()
         )
 
@@ -197,14 +201,19 @@ class Compute:
 
     def satisfies(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
         ctx = pl.SQLContext(cuallee=dataframe)
-        return ctx.execute(
-            '''
+        return (
+            ctx.execute(
+                """
             SELECT ({}) as total
             FROM cuallee
-            '''.format(rule.value),
-            eager=True,
-        ).cast(pl.Int8).sum()
-        
+            """.format(
+                    rule.value
+                ),
+                eager=True,
+            )
+            .cast(pl.Int8)
+            .sum()
+        )
 
     def has_entropy(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
         def entropy(labels):
@@ -223,43 +232,59 @@ class Compute:
 
             return -np.sum(probs * np.log(probs)) / np.log(n_classes)
 
-        return entropy(dataframe.select(pl.col(rule.column)).to_series()) == float(rule.value)
+        return entropy(dataframe.select(pl.col(rule.column)).to_series()) == float(
+            rule.value
+        )
 
     def is_on_weekday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return (
-            dataframe.select(pl.col(rule.column).dt.weekday().is_between(1, 5).cast(pl.Int8)).sum()
-        )
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().is_between(1, 5).cast(pl.Int8)
+        ).sum()
 
     def is_on_weekend(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return (
-            dataframe.select(pl.col(rule.column).dt.weekday().is_between(6, 7).cast(pl.Int8)).sum()
-        )
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().is_between(6, 7).cast(pl.Int8)
+        ).sum()
 
     def is_on_monday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(1).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(1).cast(pl.Int8)
+        ).sum()
 
     def is_on_tuesday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(2).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(2).cast(pl.Int8)
+        ).sum()
 
     def is_on_wednesday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(3).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(3).cast(pl.Int8)
+        ).sum()
 
     def is_on_thursday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(4).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(4).cast(pl.Int8)
+        ).sum()
 
     def is_on_friday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(5).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(5).cast(pl.Int8)
+        ).sum()
 
     def is_on_saturday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(6).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(6).cast(pl.Int8)
+        ).sum()
 
     def is_on_sunday(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return dataframe.select(pl.col(rule.column).dt.weekday().eq(7).cast(pl.Int8)).sum()
+        return dataframe.select(
+            pl.col(rule.column).dt.weekday().eq(7).cast(pl.Int8)
+        ).sum()
 
     def is_on_schedule(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
-        return (
-            dataframe.select(pl.col(rule.column).dt.hour().is_between(*rule.value).cast(pl.Int8)).sum()
-        )
+        return dataframe.select(
+            pl.col(rule.column).dt.hour().is_between(*rule.value).cast(pl.Int8)
+        ).sum()
 
     def is_daily(self, rule: Rule, dataframe: pl.DataFrame) -> complex:
         if rule.value is None:
@@ -267,12 +292,21 @@ class Compute:
         else:
             day_mask = rule.value
 
-        
         lower = self._value(dataframe.select(pl.col(rule.column)).min())
         upper = self._value(dataframe.select(pl.col(rule.column)).max())
-        sequence = pl.DataFrame({"ts" : pl.date_range(start=lower, end=upper, interval="1d", eager=True)})
-        sequence = sequence.filter(pl.col("ts").dt.weekday().is_in(day_mask)).to_series().to_list()
-        delivery = dataframe.filter(pl.col(rule.column).dt.weekday().is_in(day_mask)).to_series().to_list()
+        sequence = pl.DataFrame(
+            {"ts": pl.date_range(start=lower, end=upper, interval="1d", eager=True)}
+        )
+        sequence = (
+            sequence.filter(pl.col("ts").dt.weekday().is_in(day_mask))
+            .to_series()
+            .to_list()
+        )
+        delivery = (
+            dataframe.filter(pl.col(rule.column).dt.weekday().is_in(day_mask))
+            .to_series()
+            .to_list()
+        )
 
         # No difference between sequence of daily as a complex number
         return complex(len(dataframe), len(set(sequence).difference(delivery)))
@@ -281,9 +315,21 @@ class Compute:
         self, rule: Rule, dataframe: pl.DataFrame
     ) -> Union[bool, complex]:
         min_q, max_q = rule.value
-        lower = self._value(dataframe.select(pl.col(rule.column).quantile(min_q, interpolation="linear")))
-        upper = self._value(dataframe.select(pl.col(rule.column).quantile(max_q, interpolation="linear")))
-        return dataframe.select(pl.col(rule.column).is_between(lower, upper)).cast(pl.Int8).sum()
+        lower = self._value(
+            dataframe.select(
+                pl.col(rule.column).quantile(min_q, interpolation="linear")
+            )
+        )
+        upper = self._value(
+            dataframe.select(
+                pl.col(rule.column).quantile(max_q, interpolation="linear")
+            )
+        )
+        return (
+            dataframe.select(pl.col(rule.column).is_between(lower, upper))
+            .cast(pl.Int8)
+            .sum()
+        )
 
     def has_workflow(self, rule: Rule, dataframe: pl.DataFrame) -> Union[bool, int]:
         """Compliance with adjacency matrix"""
@@ -292,9 +338,21 @@ class Compute:
             group, event, order = rule.column
             groups = dataframe.partition_by(group)
             interactions = []
-            _d = compose(list, operator.methodcaller("values"), operator.methodcaller("to_dict", as_series=False))
+            _d = compose(
+                list,
+                operator.methodcaller("values"),
+                operator.methodcaller("to_dict", as_series=False),
+            )
             for g in groups:
-                pairs = list(zip(*_d(g.select(pl.col(event), pl.col(event).shift(-1).alias("target")))))
+                pairs = list(
+                    zip(
+                        *_d(
+                            g.select(
+                                pl.col(event), pl.col(event).shift(-1).alias("target")
+                            )
+                        )
+                    )
+                )
                 if result := set(pairs).difference(rule.value):
                     for t in result:
                         interactions.append(t)

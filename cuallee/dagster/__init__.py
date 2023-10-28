bd89f9a4e23bd0540bb2a0305951b198526ff52e
Herminio Vazquez <canimus@gmail.com>
2023-10-28 22:39:52 +0200

    Added dagster features

---
+++ cuallee/dagster/__init__.py
@@ -0,0 +1,27 @@
+from dagster import asset_check, AssetCheckResult
+from cuallee import Check
+import pandas as pd
+
+
+def make_dagster_checks(check: Check, asset: str, data: pd.DataFrame):
+    rules = check.rules
+    results = check.validate(data)
+    checks = []
+    for item in results.itertuples():
+
+        @asset_check(name=f"{item.rule}", asset=asset)
+        def _check():
+            return AssetCheckResult(
+                passed=(item.status == "PASS"),
+                metadata={
+                    "level" : item.level,
+                    "rows": int(item.rows),
+                    "column": item.column,
+                    "value": str(item.value),
+                    "violations": int(item.violations),
+                    "pass_rate": item.pass_rate
+                },
+            )
+
+        checks.append(_check)
+    return checks

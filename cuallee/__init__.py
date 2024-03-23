b8c79bb2f75b8590ca28b7487e7e1d672cc68615
Herminio Vazquez <canimus@gmail.com>
2024-03-23 10:43:51 +0100

    Added new version (#187)

--- cuallee/__init__.py
+++ cuallee/__init__.py
@@ -12,7 +12,7 @@ from toolz import compose, valfilter  # type: ignore
 from toolz.curried import map as map_curried
 
 logger = logging.getLogger("cuallee")
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 # Verify Libraries Available
 # ==========================
 try:

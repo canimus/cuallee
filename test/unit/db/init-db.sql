2bec9630010582e627c59b1268f6c58032080454
Daniel Saad <dsaad68@gmail.com>
2024-03-28 14:21:27 +0100

    - Added `are_unique`

--- test/unit/db/init-db.sql
+++ test/unit/db/init-db.sql
@@ -1,11 +1,13 @@
 
 -- id1: all unique
--- id2: all same
--- id3: 2 repeats
--- id4: 2 nulls
-CREATE TABLE public.test1 (id integer, id2 integer, id3 integer, id4 integer);
-INSERT INTO public.test1 VALUES (1, 2, 1, 1);
-INSERT INTO public.test1 VALUES (2, 2, 1, NULL);
-INSERT INTO public.test1 VALUES (3, 2, 2, 3);
-INSERT INTO public.test1 VALUES (4, 2, 2, NULL);
-INSERT INTO public.test1 VALUES (5, 2, 3, 5);
+-- id2: all unique
+-- id3: all same
+-- id4: all same
+-- id5: 2 repeats
+-- id6: 2 nulls
+CREATE TABLE public.test1 (id integer, id2 integer, id3 integer, id4 integer, id5 integer, id6 integer);
+INSERT INTO public.test1 VALUES (1, 6, 1, 2, 1, 1);
+INSERT INTO public.test1 VALUES (2, 7, 1, 2, 1, NULL);
+INSERT INTO public.test1 VALUES (3, 8, 1, 2, 2, 3);
+INSERT INTO public.test1 VALUES (4, 9, 1, 2, 2, NULL);
+INSERT INTO public.test1 VALUES (5, 10, 1, 2, 3, 5);

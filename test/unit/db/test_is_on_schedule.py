b3e7b4f0881bcd69a2f12a678137d4297af50621
Daniel Saad <dsaad68@gmail.com>
2024-04-03 18:47:20 +0200

    - Added `is_on_schedule`, `is_on_sunday`, `is_on_friday`, `is_on_monday`, `is_on_saturday`, `is_on_thursday`, `is_on_tuesday`, `is_on_wednesday`, `is_on_weekday` and `is_on_weekend`

---
+++ test/unit/db/test_is_on_schedule.py
@@ -0,0 +1,28 @@
+import polars as pl
+
+from cuallee import Check
+
+
+def test_positive(check: Check, postgresql, db_conn):
+    check.is_on_schedule("id2", (9, 17))
+    check.table_name = "public.test6"
+    result = check.validate(db_conn)
+    assert (result.select(pl.col("status")) == "PASS" ).to_series().all()
+
+
+
+def test_negative(check: Check, postgresql, db_conn):
+    check.is_on_schedule("id2", (16, 20))
+    check.table_name = "public.test6"
+    result = check.validate(db_conn)
+    assert (result.select(pl.col("status")) == "FAIL" ).to_series().all()
+
+
+def test_coverage(check: Check, postgresql, db_conn):
+    check.is_on_schedule("id2", (12, 17), pct=3/5)
+    check.table_name = "public.test6"
+    result = check.validate(db_conn)
+    print(result)
+    assert (result.select(pl.col("status")) == "PASS" ).to_series().all()
+    assert (result.select(pl.col("pass_rate")) == 3/5).to_series().all()
+
---
+++ test/unit/db/test_is_on_thursday.py
@@ -0,0 +1,25 @@
+import polars as pl
+
+from cuallee import Check
+
+
+def test_positive(check: Check, postgresql, db_conn):
+    check.is_on_thursday("id2", pct= 0.10)
+    check.table_name = "public.test5"
+    result = check.validate(db_conn)
+    assert (result.select(pl.col("status")) == "PASS" ).to_series().all()
+
+
+def test_negative(check: Check, postgresql, db_conn):
+    check.is_on_thursday("id2")
+    check.table_name = "public.test5"
+    result = check.validate(db_conn)
+    assert (result.select(pl.col("status")) == "FAIL" ).to_series().all()
+
+
+def test_coverage(check: Check, postgresql, db_conn):
+    check.is_on_thursday("id2", pct= 1/9)
+    check.table_name = "public.test5"
+    result = check.validate(db_conn)
+    assert (result.select(pl.col("status")) == "PASS" ).to_series().all()
+    assert (result.select(pl.col("pass_rate")) == 1/9).to_series().all()

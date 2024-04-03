e22d41943423b43c451612b69710506fa6a0afda
Daniel Saad <dsaad68@gmail.com>
2024-04-03 18:47:35 +0200

    - Added tests cases

--- test/unit/db/init-db.sql
+++ test/unit/db/init-db.sql
@@ -12,7 +12,6 @@ INSERT INTO public.test1 VALUES (3, 8, 1, 2, 2, 3);
 INSERT INTO public.test1 VALUES (4, 9, 1, 2, 2, NULL);
 INSERT INTO public.test1 VALUES (5, 10, 1, 2, 3, 5);
 
-
 CREATE TABLE public.test2 (id integer, id2 integer, id3 integer, id4 integer);
 INSERT INTO public.test2 VALUES (10, 100, 10.0, 10);
 INSERT INTO public.test2 VALUES (20, 200, 20.0, 20);
@@ -28,10 +27,27 @@ INSERT INTO public.test3 VALUES (0, 10, 10.0);
 INSERT INTO public.test3 VALUES (0, 10, 10.0);
 INSERT INTO public.test3 VALUES (0, 50, 10.0);
 
-
 CREATE TABLE public.test4 (id integer, id2 date);
 INSERT INTO public.test4 VALUES (10, '2022-04-01');
 INSERT INTO public.test4 VALUES (20, '2022-05-01');
 INSERT INTO public.test4 VALUES (30, '2022-06-01');
 INSERT INTO public.test4 VALUES (40, '2022-07-01');
 INSERT INTO public.test4 VALUES (50, '2022-08-01');
+
+CREATE TABLE public.test5 (id integer, id2 date);
+INSERT INTO public.test5 VALUES (10, '2024-04-01');
+INSERT INTO public.test5 VALUES (20, '2024-04-02');
+INSERT INTO public.test5 VALUES (30, '2024-04-03');
+INSERT INTO public.test5 VALUES (40, '2024-04-04');
+INSERT INTO public.test5 VALUES (50, '2024-04-05');
+INSERT INTO public.test5 VALUES (60, '2024-04-06');
+INSERT INTO public.test5 VALUES (70, '2024-04-07');
+INSERT INTO public.test5 VALUES (80, '2024-04-08');
+INSERT INTO public.test5 VALUES (90, '2024-04-09');
+
+CREATE TABLE public.test6 (id integer, id2 TIMESTAMP);
+INSERT INTO public.test6 VALUES (10, '2024-04-01 10:00:00');
+INSERT INTO public.test6 VALUES (20, '2024-04-01 11:00:00');
+INSERT INTO public.test6 VALUES (30, '2024-04-01 12:00:00');
+INSERT INTO public.test6 VALUES (50, '2024-04-01 13:00:00');
+INSERT INTO public.test6 VALUES (40, '2024-04-01 14:00:00');

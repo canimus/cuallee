27a3337b3fbe1973a14abe091c0c6ab400dd8818
Herminio Vazquez <canimus@gmail.com>
2024-07-07 14:38:25 +0000

    Added bio checks and logic check

--- cuallee/bio/checks.py
+++ cuallee/bio/checks.py
@@ -1,14 +1,15 @@
 from .. import Check
 import pandas as pd
-from pathlib import Path    
+from pathlib import Path
 
-class BioChecks():
+
+class BioChecks:
     def __init__(self, check: Check):
         self._check = check
         try:
             parent_path = Path(__file__).parent
             self._aminoacids = pd.read_csv(parent_path / "amino_acids.csv")
-        except:
+        except Exception:
             raise Exception("Unable to load aminoacid definitions")
 
     def is_dna(self, column: str):
@@ -18,5 +19,7 @@ class BioChecks():
 
     def is_protein(self, column: str):
         """Verifies that country codes are valid against the ISO standard 3166"""
-        self._check.has_pattern(column, rf"^[{''.join(self._aminoacids['1_letter_code'].tolist())}]*$")
+        self._check.has_pattern(
+            column, rf"^[{''.join(self._aminoacids['1_letter_code'].tolist())}]*$"
+        )
         return self._check

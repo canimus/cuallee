b34489fdc17934d7b7165f63628f9ce61f512991
Herminio Vazquez <canimus@gmail.com>
2024-09-28 22:57:35 +0200

    Added checks for ok method in check

---
+++ test/unit/class_check/test_ok.py
@@ -0,0 +1,21 @@
+import pytest
+from cuallee import Check
+import pandas as pd
+import polars as pl
+import duckdb as dk
+from pyspark.sql import SparkSession
+
+def test_pandas_ok(check: Check):
+    df = pd.DataFrame({"A": range(10)})
+    check.is_complete("A")
+    assert check.ok(df)
+
+def test_polars_ok(check: Check):
+    df = pl.DataFrame({"A": range(10)})
+    check.is_complete("A")
+    assert check.ok(df)
+
+def test_pyspark_ok(check: Check, spark: SparkSession):
+    df = spark.range(10)
+    check.is_complete("id")
+    assert check.ok(df)

5241de7d1507c09bea5ddbe21be4a2ceb57d382e
Herminio Vazquez <canimus@gmail.com>
2025-02-09 18:34:28 +0000

    Skeleton for all test cases

---
+++ utils/test_scripts.sh
@@ -0,0 +1,1 @@
+make inventory | grep cuallee | tr -d "'{}," | cut -d"." -f3- | tr -d " " | while IFS=: read -r engine_name test_name; do for engine in pyspark pandas duckdb; do mkdir -p "test/unit/engine/$engine/$engine_name/" && touch "test/unit/engine/$engine/$engine_name/test_$test_name.py"; done; done

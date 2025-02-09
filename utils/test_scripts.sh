a8ddbd433ff3b8ad65da2233a254314370096133
Herminio Vazquez <canimus@gmail.com>
2025-02-09 18:43:29 +0000

    Added all skeletons for all engines

--- utils/test_scripts.sh
+++ utils/test_scripts.sh
@@ -1,5 +1,5 @@
 make inventory | grep cuallee | tr -d "'{}," | cut -d"." -f3- | tr -d " " | while IFS=: read -r engine_name test_name; do
-    for engine in pyspark pandas duckdb; do
+    for engine in pyspark pandas duckdb bigquery polars snowpark; do
         mkdir -p "test/unit/engine/$engine/$engine_name/"
         touch "test/unit/engine/$engine/$engine_name/test_$test_name.py"
     done

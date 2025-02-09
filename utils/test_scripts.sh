f757bb4243dac0568293185b7f10ea4c953acb51
Herminio Vazquez <canimus@gmail.com>
2025-02-09 18:42:05 +0000

    Added format for utils generation

--- utils/test_scripts.sh
+++ utils/test_scripts.sh
@@ -1,1 +1,6 @@
-make inventory | grep cuallee | tr -d "'{}," | cut -d"." -f3- | tr -d " " | while IFS=: read -r engine_name test_name; do for engine in pyspark pandas duckdb; do mkdir -p "test/unit/engine/$engine/$engine_name/" && touch "test/unit/engine/$engine/$engine_name/test_$test_name.py"; done; done
+make inventory | grep cuallee | tr -d "'{}," | cut -d"." -f3- | tr -d " " | while IFS=: read -r engine_name test_name; do
+    for engine in pyspark pandas duckdb; do
+        mkdir -p "test/unit/engine/$engine/$engine_name/"
+        touch "test/unit/engine/$engine/$engine_name/test_$test_name.py"
+    done
+done

8ab0cdebb03f5e4a9bfc12237a3b61149554d748
Herminio Vazquez <canimus@gmail.com>
2025-02-09 17:37:36 +0000

    Added are unique validations

---
+++ test/unit/engine/duckdb/test_are_unique.py
@@ -0,0 +1,25 @@
+import duckdb
+
+from cuallee.core.check import Check
+
+from .base import DuckDbTestCase  # type: ignore
+
+
+class TestCase(DuckDbTestCase):
+    def test_pass(self, check: Check, db: duckdb.DuckDBPyConnection):
+        check.are_unique(("ID", "ID2"))
+        assert self.evaluate(check.validate(db))
+
+    def test_fail(self, db: duckdb.DuckDBPyConnection):
+        check = Check(table_name="DUPS")
+        check.are_unique(("ID4", "ID"))
+        assert self.evaluate(check.validate(db), passed=False)
+
+    def test_exception(self):
+        assert True
+
+    def test_parameter(self):
+        assert True
+
+    def test_coverage(self):
+        assert True
